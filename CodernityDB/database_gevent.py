e0720580533836eec94eb52d473bf7e47c0abc90
Codernity Team <contact@codernity.com>
2013-01-09 10:10:07 +0100

    gevent.coros => gevent.lock (gevent api change).

--- CodernityDB/database_gevent.py
+++ CodernityDB/database_gevent.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gevent.coros import RLock
+from gevent.lock import RLock
 
 from CodernityDB.env import cdb_environment
 
